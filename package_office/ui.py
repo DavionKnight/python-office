f884ff6415bf4883eb37036f7bc225b745434689
meteor <meteor>
2021-06-06 23:15:36 +0800

    添加异步线程，修改label背景色

--- package_office/ui.py
+++ package_office/ui.py
@@ -5,6 +5,7 @@ import os
 import sys
 import logging
 from package_office.office_main import office_main
+import threading
 
 from tkinter import *
 from tkinter.filedialog import askdirectory
@@ -78,30 +79,23 @@ class MY_GUI():
         self.d1 = StringVar()
         self.d2 = StringVar()
         self.d3 = StringVar()
-        self.tk.title("文本替换工具_v1.0 by meteor(V:kevin_fzs)")           #窗口名
+        self.tk.title("文本替换工具_v1.0 by meteor(V:kevin_fzs,18514617287)")           #窗口名
         #self.tk.geometry('320x160+10+10')                         #290 160为窗口大小，+10 +10 定义窗口弹出时的默认展示位置
         self.tk.geometry('1200x800+10+10')
 
     def selectPath(self):
         path_ = askdirectory()
         self.path.set(path_)
-#        print('参数个数为:', len(sys.argv), '个参数。')
-#        print('参数列表:', str(sys.argv))
-#        print('目标目录:', (sys.argv[1]))
-#        print('原文本:', (sys.argv[2]))
-#        print('替换为:', (sys.argv[3]))
-#        replace_dict = {sys.argv[2]:sys.argv[3]}
-        #print('Item:', sys.argv[2], 'value:' , (sys.argv[3]))
-#        office_main(sys.argv[1], replace_dict)
-
-    def office_test(self):
-        self.dir_tree_text.delete('1.0', END)
 
     def log_show(self, msg):
         self.log_text.insert(END, msg)
         self.log_text.insert(INSERT, '\n')
 
     def office_execute(self):
+        T = threading.Thread(target=self._office_execute)
+        T.start()
+
+    def _office_execute(self):
         self.log_text.delete('1.0', END)
         print('o1:', self.o1.get())
         print('d1:', self.d1.get())
@@ -138,13 +132,13 @@ class MY_GUI():
         #self.tk["bg"] = "pink"                                    #窗口背景色，其他背景色见：blog.csdn.net/chl0000/article/details/7657887
         #self.tk.attributes("-alpha",0.9)                          #虚化，值越小虚化程度越高
         #标签
-        Label(self.tk, text="请选择处理目录:").grid(row=0, column=0,sticky=W)
+        Label(self.tk, text="请选择处理目录:",fg="Black", bg="lightyellow").grid(row=0, column=0,sticky=W)
         Entry(self.tk, textvariable = self.path).grid(row = 1, column = 0)
         Button(self.tk, text = "路径选择", command = self.selectPath).grid(row = 1, column = 1)
-        Button(self.tk, text = "执行", command = self.office_execute).grid(row = 1, column = 2)
-        Button(self.tk, text = "test", command = self.office_test).grid(row = 2, column = 2)
+        Button(self.tk, text = "开始运行", command = self.office_execute,fg="ForestGreen", bg="SkyBlue").grid(row = 1, column = 2)
+#        Button(self.tk, text = "test", command = self.office_test).grid(row = 2, column = 2)
 
-        Label(self.tk, text="文件内容和文件名替换):").grid(row=2, column=0,sticky=W)
+        Label(self.tk, text="文件内容和文件名替换:",fg="Black", bg="lightyellow").grid(row=2, column=0,sticky=W)
         Entry(self.tk, textvariable=self.o1).grid(row = 4, column = 0)
         Label(self.tk, text="替换为").grid(row = 4, column = 1)
         Entry(self.tk, textvariable=self.d1).grid(row = 4, column = 2)
@@ -157,32 +151,13 @@ class MY_GUI():
         Label(self.tk, text="替换为").grid(row = 6, column = 1)
         Entry(self.tk, textvariable=self.d3).grid(row = 6, column = 2)
 
-        Label(self.tk, text="目录树").grid(row = 7, column = 0,sticky=W)
+        Label(self.tk, text="目录树:",fg="Black", bg="lightyellow").grid(row = 7, column = 0,sticky=W)
         self.dir_tree_text = Text(self.tk, width=60, height=40)  #原始数据录入框
         self.dir_tree_text.grid(row=8, column=0, rowspan=5, columnspan=3,sticky=W)
 
-        Label(self.tk, text="日志:").grid(row=0, column=4,sticky=W)
+        Label(self.tk, text="日志:",fg="Black", bg="lightyellow").grid(row=0, column=4,sticky=W)
         self.log_text = Text(self.tk, width=100, height=50)  #原始数据录入框
         self.log_text.grid(row=1, column=4, rowspan=60, columnspan=60, sticky=N+W)
-#        plt.grid(linestyle=":", color="r")
-#        w = Canvas(self.tk, width=200, height=200, background="white" )
-#        w.pack()
-#        w.create_line(100, 0,100, 200,fill='red', dash=(4, 4))
-#        self.result_data_label = Label(self.tk, text="目录树")
-#        self.result_data_label.grid(row=2, column=12)
-#        self.log_label = Label(self.tk, text="日志")
-#        self.log_label.grid(row=12, column=0)
-#        #文本框
-#        self.init_data_Text = Text(self.tk, width=67, height=35)  #原始数据录入框
-#        self.init_data_Text.grid(row=2, column=0, rowspan=10, columnspan=10)
-#        self.result_data_Text = Text(self.tk, width=70, height=49)  #处理结果展示
-#        self.result_data_Text.grid(row=1, column=12, rowspan=15, columnspan=10)
-#        self.log_data_Text = Text(self.tk, width=66, height=9)  # 日志框
-#        self.log_data_Text.grid(row=13, column=0, columnspan=10)
-#        #按钮
-#        self.str_trans_to_md5_button = Button(self.tk, text="字符串转MD5", bg="lightblue", width=10,command=self.str_trans_to_md5)  # 调用内部方法  加()为直接调用
-#        self.str_trans_to_md5_button.grid(row=1, column=11)
-
 
     #功能函数
     def str_trans_to_md5(self):
